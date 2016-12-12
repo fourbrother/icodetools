523521a87fd298654fd9c09f2af701339f1d5cdc
CORP\jiangwei1-g <1095639004@qq.com>
2016-12-12 16:04:51 +0800

    增加工具运行环境

--- icodetools_1.0.bat
+++ icodetools_1.0.bat
@@ -1,5 +1,5 @@
 cd %~dp0
 set aapt_path=D:\Android_tools\AndroidSdk\build-tools\23.0.1\aapt.exe
-java -jar libs\icodetools_1.0.jar %~dp0 src.apk %aapt_path% jw cn.wjdiankong.jw.utils.JWUtils printStackTrace
+java -Xmx2048m -XX:-UseParallelGC -XX:MinHeapFreeRatio=15 -jar libs\icodetools_1.0.jar %~dp0 src.apk %aapt_path% jw cn.wjdiankong.jw.utils.JWUtils printStackTrace
 adb install -r signed.apk
 pause..
